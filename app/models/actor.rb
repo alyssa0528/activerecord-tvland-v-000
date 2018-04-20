7a831d3e07b19a6801820504915e344aa37873c0
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-20 12:25:04 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -10,7 +10,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    binding.pry
+    #binding.pry
     self.characters.collect do |characters|
       "#{characters[:name]} - #{characters[:name].show}"
     end
