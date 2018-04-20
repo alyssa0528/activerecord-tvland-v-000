bfe6b851b98dc1fb8f8abf29caeb6ded5b038cdf
Alyssa Kim <alyssa.kim@gmail.com>
2018-04-20 09:56:30 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,8 +1,8 @@
-class Characters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
-      t.integer :show_id 
+      t.integer :show_id
     end
   end
 end
